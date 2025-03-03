bef269563c7af04662a478f658121ae90710ecbe
danil-emelin <danil228>
2025-03-03 08:36:39 -0500

    finish classes

--- src/main/java/org/example/task3/Consciousness.java
+++
@@ -1,6 +0,0 @@
-package org.example.task3;
-
-// for creatures ,having brains and ability to think
-public interface Consciousness {
-    int calculateIntelligence();
-}
--- src/main/java/org/example/task3/Universe.java
+++ src/main/java/org/example/task3/Universe.java
@@ -1,13 +1,4 @@
 package org.example.task3;
 
-
-
-import java.util.List;
-
-
-record Universe(String name, List<Creature> beings) {
-    public void addBeing(Creature being) {
-        beings.add(being);
-    }
-
+public class Universe {
 }
