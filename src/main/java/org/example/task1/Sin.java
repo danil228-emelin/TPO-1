5ed0a735c671576d2adf38d7357923a0b003a2ee
danil-emelin <danil228>
2025-03-03 03:13:26 -0500

    create Sin class

---
+++ src/main/java/org/example/task1/Sin.java
@@ -0,0 +1,39 @@
+package org.example.task1;
+
+public class Sin {
+
+    // https://function-x.ru/chapter9-4/rows4_clip_image063.gif
+
+    public static double calc(double x, int n) {
+        double PI2 = Math.PI * 2;
+
+        if (x >= 0) {
+            while (x > PI2) {
+                x -= PI2;
+            }
+        } else if (x < 0) {
+            while (x < PI2) {
+                x += PI2;
+            }
+        }
+
+        double result = 0;  // Хранится результат
+
+        double xx = x * x;  // x^2
+        double pow = x;     // Для Вычисления числителя
+        double fact = 1;    // Для вычисления факториала знаменателя
+
+        int sign = 1;       // Отвечает за знак (чередование '+' -> '-' -> '+' -> ...)
+
+        for (int i = 1; i < n; i += 2) {
+            fact /= i;
+            result += sign * pow * fact;    // (-1)^(n-1) * x^(2n-1) / (2n-1)!
+            sign = -sign;
+            fact /= (i + 1);
+            pow *= xx;
+        }
+
+        // Дополнительно округляем, чтобы легче было проверять
+        return result;
+    }
+}
